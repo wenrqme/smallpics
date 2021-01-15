758d3bde502a5f5d165495e1b4243c9a08433c53
wenrqme <wenrqme@gmail.com>
2021-01-15 01:16:01 -0500

    preserve color profile

--- resize.py
+++ resize.py
@@ -21,16 +21,17 @@ for file in glob.glob(IN_FOLDER + '*'):
     file_base = os.path.basename(file)[:-4]
     output_file = OUT_FOLDER + '/' + file_base + '.jpg'
     print(file_base)
-    
+
     #only resize if images are not already the directory
     if not os.path.exists(output_file):
         print("resizing!")
         img = Image.open(file)
+        icc_profile = img.info.get('icc_profile')
         w_percent = width/float(img.size[0])
         height = int(float(img.size[1])*float(w_percent))
         new_img = img.resize((width, height), Image.ANTIALIAS)
         new_img = new_img.convert('RGB')
-        new_img.save(output_file)
+        new_img.save(output_file, format='JPEG', icc_profile=icc_profile)
     else:
         print('already here, not resizing!')
 
